f9a125ac63f1adc1e015258868b1a99aa82bc714
Razibul-Islam <razibulislam665@gmail.com>
2025-09-12 21:42:19 +0600

    added two new function

--- src/StreamingPayment.sol
+++ src/StreamingPayment.sol
@@ -14,6 +14,9 @@ contract StreamingPayment is Ownable, ReentrancyGuard {
     error Streaming__InvalidIntervalAmount();
     error Streaming__NotProvider();
     error Streaming__ActiveServices();
+    error Streaming__NotSubscriber();
+    error Streaming__ServiceIsNotAvailable();
+    error Streaming__ServiceIsNotActive();
 
     enum Role {
         Subscriber,
@@ -36,8 +39,17 @@ contract StreamingPayment is Ownable, ReentrancyGuard {
         Status subcriptionStatus;
     }
 
+    struct Service {
+        address provider;
+        uint256 servicePrice;
+        uint256 serviceDuration;
+        bool isActive;
+        uint256 activeSubscriber;
+    }
+
     mapping(address => Role) public userRole;
     mapping(address => Subscription) public subscriptions;
+    mapping(address => Service[]) public services;
     mapping(address => Status) public subscriptionStatus;
     mapping(address => bool) public subscriptionPerUser;
     mapping(address => uint256) public hasActiveSubscriber;
@@ -52,8 +64,13 @@ contract StreamingPayment is Ownable, ReentrancyGuard {
         uint256 startInterval
     );
     event ProviderDowngraded(address provider);
+    event ServiceCreated(
+        uint256 servicePrice,
+        uint256 serviceDuration,
+        address provider
+    );
 
-    uint256 public constant AMOUNT_PER_INTERVAL = 0.0002 ether;
+    uint256 public constant SERVICE_PRICE = 0.02 ether;
     uint256 public constant AMOUNT_WHEN_BECOME_PROVIDER = 0.0001 ether;
 
     constructor() Ownable(msg.sender) {
@@ -88,5 +105,56 @@ contract StreamingPayment is Ownable, ReentrancyGuard {
 
         userRole[msg.sender] = Role.Subscriber;
         emit ProviderDowngraded(msg.sender);
+    }
+
+    function createService(uint256 duration) external {
+        require(
+            userRole[msg.sender] == Role.Provider,
+            Streaming__NotProvider()
+        );
+        require(duration > 0, Streaming__InvalidInterval());
+
+        services[msg.sender].push(
+            Service({
+                provider: msg.sender,
+                servicePrice: SERVICE_PRICE,
+                serviceDuration: duration,
+                isActive: true,
+                activeSubscriber: 0
+            })
+        );
+
+        emit ServiceCreated(SERVICE_PRICE, duration, msg.sender);
+    }
+
+    function buyService(uint256 serviceId, address provider) external payable {
+        require(
+            userRole[msg.sender] == Role.Subscriber,
+            Streaming__NotSubscriber()
+        );
+        require(
+            serviceId < services[provider].length,
+            Streaming__ServiceIsNotAvailable()
+        );
+        require(
+            services[provider][serviceId].isActive,
+            Streaming__ServiceIsNotActive()
+        );
+        require(
+            msg.value == services[provider][serviceId].servicePrice,
+            Streaming__NotEnoughBalance()
+        );
+        services[provider][serviceId].activeSubscriber += 1;
+
+        subscriptions[msg.sender] = Subscription({
+            userAddress: msg.sender,
+            providerAddress: provider,
+            price: services[provider][serviceId].servicePrice,
+            duration: services[provider][serviceId].serviceDuration,
+            nextPayment: services[provider][serviceId].serviceDuration +
+                block.timestamp,
+            ServiceStatus: Status.Active,
+            subcriptionStatus: Status.Active
+        });
     }
 }
